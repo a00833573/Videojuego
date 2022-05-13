1979054045132959f6e7fbbdd8d39b609e6cf49b
JavHZP <105375472+JavHZP@users.noreply.github.com>
2022-05-13 10:50:32 -0500

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -24,26 +24,49 @@ def posicion_correcta(posicion):
             return True
     return False
 
-def
+def actualizar_tablero(posicion,jugador):
+           tablero[posicion[0]][posicion[1]] = jugador
+           
+def ha_ganado(j):
+           if tablero[0] == [j,j,j] or tablero[1] == [j,j,j] or tablero[2] == [j,j,j]
+                 return True
+           elif tablero[0][0] == j and tablero[1][0] == j and tablero[2][0] == j
+                 return True
+           elif tablero[0][1] == j and tablero[1][1] == j and tablero[2][1] == j
+                 return True
+           elif tablero[0][2] == j and tablero[1][2] == j and tablero[2][2] == j
+                 return True
+           elif tablero[0][0] == j and tablero[1][1] == j and tablero[2][2] == j
+                 return True
+           elif tablero[0][2] == j and tablero[1][1] == j and tablero[2][0] == j
+                 return True
+           return False
 
 def juego():
     mostrar_tablero()
     jugador = rotar_turno()
     while True:
-        input("Turno de jugador {}. Elije la posición fila,columna de 1 a 3".format(jugador))
+        input("Turno de jugador {}. Elije la posición fila,columna de 1 a 3: ".format(jugador))
         if posicion == 'Salir':
             break
-        jugador = rotar_turno()
+       
         try:
             posicion_1 = procesar_posicion(posicion)
         except:
             print)"Error, posicion {} no es valida".format(posicion)
             continue
         if posicion_correcta(posicion_1):
-            print("correcta")
-            actualizar_tablero(posicion_1,jugador) 
+           print("correcta")
+           actualizar_tablero(posicion_1,jugador) 
+           mostrar_tablero()
+           
+           if ha_gando(jugador):
+               print("Jugador de {} ha ganado!!!!".format(jugador))
+               break
+           jugador = rotar_turno()
+              
+           
         else:
             print("incorrecta")
         
 juego()
-#continuamos
