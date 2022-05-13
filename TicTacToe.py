e8046ea259ada206d447f9343ae1d29d2e7e38e5
a00833573 <105307704+a00833573@users.noreply.github.com>
2022-05-13 10:16:58 -0500

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,24 @@
+from collection import deque
+
+turno = deque(["1","2"])
+tablero = [ [" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
+def rotar_turno():
+    turno.rotate()
+    return turno[0]
+
+def mostrar_tablero():
+    print ("")
+    for fila in tablero:
+        print (fila)
+
+def juego():
+    mostrar_tablero()
+    jugador = rotar_turno()
+    while True:
+        input("Turno de jugador {}. Elije la posición fila,columna de 1 a 3".format(jugador))
+        if posicion == 'Salir':
+            break
+        jugador = rotar_turno()
+        
+juego()
